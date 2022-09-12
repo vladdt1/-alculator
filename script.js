2b2153b83dc01df6dfa0e90fc8c12105fe60ca16
vladdt1 <vladtitov2018@mail.ru>
2022-09-12 20:13:30 +0300

    1.0

---
+++ script.js
@@ -0,0 +1,138 @@
+let input = document.querySelector('.input');
+//Сохраненная часть выражения для возведения в степень
+let power = "";
+//Вставить символ
+function insert(num) {
+    if (input.textContent == "Infinity"){
+        clean();
+    }
+    if (input.textContent == 0) {
+        if(input.textContent){
+        input.textContent = "";
+        input.textContent += num;
+        }
+    } else{
+        if(input.textContent){
+        input.textContent += num;
+        }
+    }
+            
+}
+//Очистить все поле
+function clean() {
+    input.textContent = "0";
+    power = "";
+}
+//Удалить символ
+function back() {
+    let exp = input.textContent;
+    input.textContent = exp.substring(0, exp.length - 1);
+    if (input.textContent == 0) {
+        input.textContent = "0";
+    }
+}
+
+function plusminus(){
+    let exp = input.textContent;
+    if((exp)>0){
+        input.textContent = "-"+ input.textContent;
+        return;
+    }else{
+        exp = exp * (-1);
+        input.textContent = exp;
+        return;
+    }
+}
+//Переключение с градусов на радианы
+document.querySelector('.type').addEventListener('click', function() {
+    if (document.querySelector('.type').textContent == "deg") {
+        this.textContent = "rad";
+        console.log('Градусы')
+    } else if (document.querySelector('.type').textContent == "rad") {
+        this.textContent = "deg";
+        console.log('Радианы')
+    }
+})
+//Посчитать выражение
+function equal() {
+    let exp = input.textContent;
+    if (input.textContent.includes('^')) {
+        let tmp = input.textContent.split('^')
+        let num = eval(power);
+        let pow = +tmp[1]
+        input.textContent = +Math.pow(num, pow).toFixed(8);
+        power = "";
+        return;
+    }
+    if (exp) {
+        input.textContent = +eval(exp).toFixed(8);
+    }
+}
+//Константа
+function constant(name) {
+    if (input.textContent == 0) {
+        input.textContent = "";
+    }
+    if (name == "pi")
+        input.textContent += Math.PI.toFixed(8);
+}
+
+function operation(name) {
+    if (name == "sqrt")
+        input.textContent = +Math.sqrt(eval(input.textContent)).toFixed(8);
+    if (name == "^-1")
+        input.textContent = +Math.pow(eval(input.textContent), -1).toFixed(8);
+    if (name == "^") {
+        power = input.textContent;
+        input.textContent += "^";
+    }
+}
+
+function log(name) {
+    if (name == 'lg') {
+        input.textContent = +Math.log10(eval(input.textContent)).toFixed(8);
+    }
+    if (name == 'ln') {
+        input.textContent = +Math.log(eval(input.textContent)).toFixed(8);
+    }
+}
+
+function f(name) {
+    if (name == 'sin') {
+        if(document.querySelector('.type').textContent == "deg") {
+            //В градусах
+            input.textContent = +parseFloat(Math.sin(eval(input.textContent) / 180 * Math.PI)).toFixed(8).toString();
+        } else {
+            //В радианах
+            input.textContent = +parseFloat(Math.sin(eval(input.textContent))).toFixed(8).toString();
+        }        
+    }
+    if (name == 'cos') {
+        if(document.querySelector('.type').textContent == "deg") {
+            input.textContent = +parseFloat(Math.cos(eval(input.textContent) / 180 * Math.PI)).toFixed(8).toString();
+        } else {
+            input.textContent = +parseFloat(Math.cos(eval(input.textContent)).toFixed(8).toString());
+        } 
+    }
+    if (name == 'tan') {
+        if(document.querySelector('.type').textContent == "deg") {
+            input.textContent = +parseFloat(Math.tan(eval(input.textContent) / 180 * Math.PI)).toFixed(8).toString();
+        } else {
+            input.textContent = +parseFloat(Math.tan(eval(input.textContent))).toFixed(8).toString();
+        }  
+    }
+    if (name == 'asin') {
+        if(document.querySelector('.type').textContent == "deg") {
+            input.textContent = +parseFloat(1/Math.sin(eval(input.textContent) / 180 * Math.PI)).toFixed(8);
+        } else {
+            input.textContent = +parseFloat(1/Math.sin(eval(input.textContent))).toFixed(8).toString();
+        } 
+    }
+    if (name == 'acos') {
+        if(document.querySelector('.type').textContent == "deg") {
+            input.textContent = +parseFloat(1/Math.cos(eval(input.textContent) / 180 * Math.PI)).toFixed(8).toString();
+        } else {
+            input.textContent = +parseFloat(1/Math.cos(eval(input.textContent))).toFixed(8).toString();
+        } 
+    }
+}
